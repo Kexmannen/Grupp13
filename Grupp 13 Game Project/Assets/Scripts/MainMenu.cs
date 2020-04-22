8bb07be26d2cdcfaaae8a947c4adbe4029fa36bd
Kexmannen <63399967+Kexmannen@users.noreply.github.com>
2020-04-22 19:26:10 +0200

    laddar upp mina ändringar round2 here we go

---
+++ Grupp 13 Game Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        //Detta laddar nästa scen i build index, vi kan sätta det så det blir ett fast värde, men gjorde detta så länge.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
