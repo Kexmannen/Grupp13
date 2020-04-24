20a50d03517b37013ee6da57982bec8a0daf8752
Kexmannen <63399967+Kexmannen@users.noreply.github.com>
2020-04-24 16:08:26 +0200

    Revert "xzxs"
    This reverts commit 8c3cc1efb1d576099ebae841a59037f51ff20f4b, reversing
    changes made to ac36c5b290bab16d8f5dd2dd86be9eff4d649f85.

--- Grupp 13 Game Project/Assets/Scripts/MainMenu.cs
+++ Grupp 13 Game Project/Assets/Scripts/MainMenu.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -12,16 +11,10 @@ public class MainMenu : MonoBehaviour
     public AudioSource source;
     public AudioClip buttonPress;
     public int delay;
-=======
-
-public class MainMenu : MonoBehaviour
-{
->>>>>>> master
 
     public void PlayGame()
     {
         //Detta laddar nästa scen i build index, vi kan sätta det så det blir ett fast värde, men gjorde detta så länge.
-<<<<<<< HEAD
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         StartCoroutine(ChangeLevel());
     }
@@ -38,8 +31,4 @@ public class MainMenu : MonoBehaviour
 
     }
 
-=======
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
->>>>>>> master
 }
